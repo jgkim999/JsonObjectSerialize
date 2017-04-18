27cd8fe2e6349e848699a845ce59e318820b841f
jgkim <jgkim999@gmail.com>
2017-04-18 11:20:37 +0900

    Object to json serialize and deserialize example

---
+++ JsonObjectSerialize/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace JsonObjectSerialize
+{
+    public class Employee
+    {
+        public string firstName_;
+        public string lastName_;
+    }
+
+    public class Boss
+    {
+        public int employeeId_;
+        public string name_;
+    }
+
+    public class People
+    {
+        public List<object> object_;
+    }
+}
---
+++ JsonObjectSerialize/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace JsonObjectSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People();
+            people.object_ = new List<object>();
+            people.object_.Add(new Employee { firstName_ = "kim", lastName_ = "jg" });
+            people.object_.Add(new Boss { employeeId_ = 1, name_ = "you" });
+            people.object_.Add(new List<int>() { 1, 2 });
+            people.object_.Add(new Dictionary<string, int>() { { "aa", 1 }, { "bb", 2 } });
+
+            var jsonSerializeSettings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            var jsonStr = JsonConvert.SerializeObject(people, jsonSerializeSettings);
+
+            People what = JsonConvert.DeserializeObject<People>(jsonStr, jsonSerializeSettings);
+            var intlist = what.object_[2] as List<int>;
+            var dic = what.object_[3] as Dictionary<string, int>;
+
+            Console.WriteLine(intlist);
+            Console.WriteLine(dic);
+
+        }
+    }
+}
